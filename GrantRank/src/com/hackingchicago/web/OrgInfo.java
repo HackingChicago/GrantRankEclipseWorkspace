e26ff6824608f4edef6e0367f6f170925c210f56
Eric Chiu <wf870125@gmail.com>
2017-02-19 11:17:05 -0600

    Update path to res

--- GrantRank/src/com/hackingchicago/web/OrgInfo.java
+++ GrantRank/src/com/hackingchicago/web/OrgInfo.java
@@ -2,12 +2,15 @@ package com.hackingchicago.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.hackingchicago.web.utils.Utils;
 
 /**
  * Servlet implementation class OrgInfo
@@ -45,7 +48,7 @@ public class OrgInfo extends HttpServlet {
         // TODO Auto-generated method stub
         
         
-        PrintWriter out = response.getWriter();
+        /*PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<body>");
         out.println("<h1>Organization Info</h1>");
@@ -68,7 +71,9 @@ public class OrgInfo extends HttpServlet {
         out.println("</td><td></td><tr>");
         out.println("</form>");
         out.println("</body>");
-        out.println("</html>");
+        out.println("</html>");*/
+        String content = Utils.readFile("org-info.html", StandardCharsets.UTF_8);
+		response.getWriter().append(content);
     }
 	
 	
